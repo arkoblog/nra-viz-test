4b8943004ba0143e50fbd1e477fcdfb7077168cf
akoira <akoira>
2017-03-26 15:08:04 +0545

    Added about section

--- nra-dashboard/app/components/Sidebar.js
+++ nra-dashboard/app/components/Sidebar.js
@@ -59,20 +59,40 @@ var Sidebar = React.createClass({
 			            <div className="sidebar-pane " id="info">
 			                <h1 className="sidebar-header">
 			                    <span className="sidebar-close"><i className="fa fa-remove"></i></span>
-			                	ABOUT THIS TOOL
+			                	WELCOME
 			                </h1>
 			                <div className="row-fluid ">
 			                <div className="content-row info-text col-md-12 col-lg-12 col-sm-12 ">
-			                <div className="row-fluid bar-header">WELCOME</div>
-			                <div className="row-fluid info-content">
-			                <p>Nullam vel vulputate massa, vitae mollis tellus. Praesent tristique est id massa consectetur, sed finibus velit cursus. 
-			                	Sed rutrum consectetur vestibulum. Aenean elit leo, placerat imperdiet tincidunt et, efficitur eu lorem. 
-			                	Pellentesque scelerisque, erat ac accumsan vestibulum, mi odio pretium ipsum, at varius diam ante eget augue. Duis quis lacinia libero. 
-			                	Vivamus urna tortor, ultricies et libero ac, pharetra gravida mi. Praesent scelerisque eu libero in congue.</p>
-			                <p>Sed in hendrerit purus. Nam ultrices volutpat leo non rhoncus. Aliquam mollis sem non neque feugiat, facilisis porttitor sem luctus. 
-			                Etiam risus diam, efficitur et erat nec, vulputate tincidunt arcu. Ut in pulvinar mauris, quis placerat lorem. 
-			                Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque varius, nulla eget rhoncus dapibus, lectus velit ultrices tortor, non cursus lorem nulla ut justo.</p>
-			                </div>
+							<div className="row-fluid info-content">
+
+								<span className="bar-header-about">About this website</span><br/><br/>
+								<div className="row-fluid  ">
+
+									<p>This website shows the latest statistics of the Housing Reconstruction Assessment survey being conducted by the National Reconstruction Authority (NRA) of Nepal.</p>
+
+									<p>To view the statistics, click on the   <i className="fa fa-area-chart"></i>   icon in the top-right corner.</p><br/>
+								</div>
+							</div>
+							<div className="row-fluid info-content">
+
+								<span className="bar-header-about">About this Survey</span><br/><br/>
+								<div className="row-fluid  ">
+
+									<p>The National Reconstruction Authority (NRA) has deployed volunteers to assess the status of housing reconstruction in the earthquake-affected districts of Nepal. These volunteers go to field and interview the house owners identified as beneficiaries by the National Housing Reconstruction Program survey.</p>
+
+									<p>The main objectives of this survey are as follows:
+									</p>
+									<ol>
+										<li>To ascertain the number of reconstructed houses, i.e. how many houses have been reconstructed, how many are being reconstructed and how many are yet to be reconstructed?</li>
+										<li>To ascertain the status of reconstructed houses, i.e. what percentage of the reconstructed houses have followed the building codes?</li>
+										<li>To identify the actual issues, if any, which are preventing the reconstruction efforts on field (for example, issues like lack of skilled manpower, lack of construction materials, etc.)</li>
+
+									</ol>
+									<p>In its current phase, about 1,000 volunteers (final year engineering students from Pokhara University) are conducting this survey in five districts (Dolakha, Dhading, Gorkha, Nuwakot and Sindhupalchowk).
+									</p>
+
+								</div>
+							</div>
 			                </div>
 			                </div>
 			            </div>
